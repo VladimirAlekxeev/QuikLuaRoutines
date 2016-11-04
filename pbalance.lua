510fea4c069258a888c62e13a93749b73679284f
Mikhail Smirnov <mike-smirnoff@yandex.ru>
2016-11-04 21:34:32 +0400

    Убрал лишнее

--- pbalance.lua
+++ pbalance.lua
@@ -4,8 +4,6 @@ package.path = gPath .. "\\?.lua;" .. package.path
 local core = require "core"
 local const = require "constants"
 	
-is_run = true
-
 function main()	
 	local portfolio_securities = {
 		"ALRS",
@@ -21,8 +19,4 @@ function main()
 	}
 	local advises = core.GetAdvises(const.firm_id, const.client_code, const.class_code, portfolio_securities)
 	core.ShowAdvisesMsg(advises)
-end
-
-function OnStop()
-	is_run = false
 end
